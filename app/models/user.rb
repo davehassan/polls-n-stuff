ff1c5dd5c2538ef108d0dcd5c681713f23d1416d
DamonAndDave <fake@thisisfake.org>
2015-10-15 16:50:58 -0400

    Add some queries n stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,49 @@ class User < ActiveRecord::Base
     class_name: "Response",
     foreign_key: :responder_id,
     primary_key: :id)
+
+  # <<-SQL
+  # SELECT
+  #   polls.*, COUNT(questions.id) AS q_per_pol
+  # FROM
+  #   polls
+  # JOIN
+  #   questions
+  # ON questions.poll_id = polls.id
+  # LEFT OUTER JOIN
+  #   (SELECT
+  #     *
+  #   FROM
+  #     responses
+  #   JOIN
+  #     answer_choices ON responses.answer_id = answer_choices.id
+  #   WHERE
+  #     responses.responder_id = ?) AS my_responses
+  # ON
+  #   my_responses.question_id = questions.id
+  # GROUP BY
+  #   polls.id
+  # HAVING
+  #   q_per_pol = COUNT(my_responses.id)
+  # SQL
+  #
+  #
+  # <<-SQL
+  # SELECT
+  # FROM
+  #   polls
+  # JOIN
+  #   questions AS qs_for_poll
+  # ON questions.poll_id = polls.id
+  # JOIN
+  #   answer_choices AS
+  # SQL
+  #
+  # def completed_polls
+  #   Poll
+  #     .select("polls.*, COUNT(questions.id) AS q_per_pol")
+  #     .joins("JOIN questions ON questions.poll_id = polls.id")
+  #     .joins("LEFT OUTER JOIN ")
+  # end
+
 end
